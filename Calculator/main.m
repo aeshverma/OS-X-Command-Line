07996f07298477fa452391288939d870588a1c51
aeshverma <aeshverma@gmail.com>
2013-11-25 01:09:02 -0500

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Aeshvarya Verma on 11/25/13.
+//  Copyright (c) 2013 Aeshvarya Verma. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
